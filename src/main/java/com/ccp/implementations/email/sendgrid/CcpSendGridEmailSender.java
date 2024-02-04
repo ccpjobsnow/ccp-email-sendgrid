4b6b7a962ee3b5d53378a39b77e351cc8cb3deb9
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-02-04 17:20:14 -0300

    Validações genéricas nos fields dos Jsons dos endpoints

--- src/main/java/com/ccp/implementations/email/sendgrid/CcpSendGridEmailSender.java
+++ src/main/java/com/ccp/implementations/email/sendgrid/CcpSendGridEmailSender.java
@@ -4,7 +4,7 @@ import com.ccp.dependency.injection.CcpInstanceProvider;
 
 public class CcpSendGridEmailSender implements CcpInstanceProvider {
 
-	@Override
+	
 	public Object getInstance() {
 		return new SendGridEmailSender();
 	}
